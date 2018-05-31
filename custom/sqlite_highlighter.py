17aae063251087a12767b8f19f6b0c16ef28e8ac
HamanoTaiki <Hamano0813@gmail.com>
2018-05-31 10:20:32 +0800

    update for regex style with sql statement

--- custom/sqlite_highlighter.py
+++ custom/sqlite_highlighter.py
@@ -9,7 +9,7 @@ class SQLiteHighlighter(QSyntaxHighlighter):
     rules = []
     normal_fmt = QTextCharFormat()
     normal_fmt.setFont(QFont('Consolas'))
-    normal_fmt.setFontWeight(QFont.Bold)
+    # normal_fmt.setFontWeight(QFont.Bold)
     normal_fmt.setFontPointSize(11)
 
     def __init__(self, parent=None):
@@ -208,7 +208,7 @@ class SQLiteHighlighter(QSyntaxHighlighter):
 
         kw_fmt = QTextCharFormat(self.normal_fmt)
         kw_fmt.setForeground(Qt.blue)
-        kw_fmt.setFontWeight(QFont.ExtraBold)
+        kw_fmt.setFontWeight(QFont.Bold)
         func_fmt = QTextCharFormat(self.normal_fmt)
         func_fmt.setForeground(Qt.darkGreen)
         cmt_fmt = QTextCharFormat(self.normal_fmt)
@@ -218,11 +218,17 @@ class SQLiteHighlighter(QSyntaxHighlighter):
         name_fmt.setForeground(Qt.red)
         tname_fmt = QTextCharFormat(self.normal_fmt)
         tname_fmt.setForeground(Qt.magenta)
-        tname_fmt.setFontWeight(QFont.ExtraBold)
+        value_fmt = QTextCharFormat(self.normal_fmt)
+        value_fmt.setForeground(Qt.darkBlue)
+        text_fmt = QTextCharFormat(self.normal_fmt)
+        text_fmt.setForeground(Qt.darkYellow)
 
+        self.rules.append((QRegExp('\\b(\-|\+)?\d+(\.\d+)?\\b'), value_fmt))
+        self.rules.append((QRegExp('\\b[a-zA-Z]\\w*\\b'), name_fmt))
         self.rules.extend([(QRegExp(f'\\b{pattern}\\b', Qt.CaseInsensitive), tname_fmt) for pattern in typenames])
         self.rules.extend([(QRegExp(f'\\b{pattern}\\b', Qt.CaseInsensitive), kw_fmt) for pattern in keywords])
         self.rules.extend([(QRegExp(f'\\b{pattern}(?=\\()', Qt.CaseInsensitive), func_fmt) for pattern in functions])
+        self.rules.append((QRegExp('''(".*"|'.*')'''), text_fmt))
         self.rules.append((QRegExp('\\[([^\\[\\]]*)\\]'), name_fmt))
         self.rules.append((QRegExp('--(\\w|\\W|[\\u4e00-\\u9fa5])*$', Qt.CaseInsensitive), cmt_fmt))
 
